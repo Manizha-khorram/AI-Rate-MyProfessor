e172557f8f279394d5077bc45c0267f5cab28f2e
Manizha-khorram <108601629+Manizha-khorram@users.noreply.github.com>
2024-08-27 01:03:11 -0400

    signin page problem

--- app/page.js
+++ app/page.js
@@ -1,21 +1,20 @@
-'use client';
+"use client";
 
-import LandingPage from './landing/page';
-import SigninPage from './signin/page';
-import SignupPage from './signup/page';
-import { usePathname } from 'next/navigation';
+import LandingPage from "./landing/page";
+import LoginPage from "./signIn/page";
+import SignupPage from "./signup/page";
+import { usePathname } from "next/navigation";
 
 const Page = () => {
   const pathname = usePathname();
-    if (pathname == '/signup') {
-      return <LandingPage />;
-    }
-    if (pathname == '/signin') {
-      return <SigninPage />;
-    }
-  
-    return <SignupPage />;
-   
+  if (pathname == "/signup") {
+    return <LandingPage />;
+  }
+  if (pathname == "/signIn") {
+    return <LoginPage />;
+  }
+
+  return <SignupPage />;
 };
 
 export default Page;
